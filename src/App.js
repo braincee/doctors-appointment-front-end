965a402269c23b2d6b13063e8da9a2d8c089c076
braincee <kwesi938@gmail.com>
2022-11-26 18:20:39 +0000

    created redux store and added it to APP.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
+import React, { Component } from 'react';
 import './App.css';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Component } from 'react';
 
-function App() {
-  return (
+class App extends Component {
+  render() {
+    return (
+    <Provider store={store}>
     <div className="App">
 
     </div>
+    </Provider>
+
   );
+
+   }
 }
 
 export default App;
