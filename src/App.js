efcaf8fc1275d429cd6e3ddbdc2ccf6f129516e5
braincee <kwesi938@gmail.com>
2022-11-30 20:30:01 +0000

    add form control

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
 import Register from './components/Register/Register';
+import Login from './components/Login/Login';
 
 function App() {
   return (
-    <div className="App">
-      <Register />
-    </div>
+    <Routes>
+      <Route exact path="/login" element={<Login />} />
+      <Route exact path="/signup" element={<Register />} />
+    </Routes>
   );
 }
 
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,28 +1,25 @@
-import React from 'react'
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import React from 'react';
 
-const Login = () => {
-  return (
-    <div className="container-fluid w-50 mx-auto mt-5"> 
-      <h2 className="h2 mt-5 mb-3 text-center">Log in</h2>
-      <form>
-        
-        <div className="form-floating mb-3">
-          <input
-            type="text"
-            className="form-control"
-            id="username"
-            placeholder="Username"
-            value={username}
-            required
-          />
-          <label htmlFor="username">Username</label>
-        </div>
-        <button type="submit" className="btn btn-primary">
-          Log in
-        </button>
-      </form>
-    </div>
-  )
-}
+const Login = () => (
+  <div className="container-fluid w-50 mx-auto mt-5">
+    <h2 className="h2 mt-5 mb-3 text-center">Log in</h2>
+    <form>
+      <div className="form-floating mb-3">
+        <input
+          type="text"
+          className="form-control"
+          id="UserName"
+          placeholder="Username"
+          required
+        />
+        <label htmlFor="UserName">Username</label>
+      </div>
+      <button type="submit" className="btn btn-primary">
+        Log in
+      </button>
+    </form>
+  </div>
+);
 
-export default Login
+export default Login;
