4cafd39fe04855bab38be53d9f13b97fd2b80b23
braincee <kwesi938@gmail.com>
2022-12-01 02:29:18 +0000

    Add splash screen route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
+import SplashScreen from './components/SplashScreen/SplashScreen';
 import Register from './components/Register/Register';
 import Login from './components/Login/Login';
 
 function App() {
   return (
     <Routes>
+      <Route exact path="/" element={<SplashScreen />} />
       <Route exact path="/login" element={<Login />} />
       <Route exact path="/signup" element={<Register />} />
     </Routes>
