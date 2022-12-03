ec7834ac63e262c3d8bca21e26463441c4fbcb91
braincee <kwesi938@gmail.com>
2022-12-03 17:59:13 +0000

    Add get and delete reducers

--- src/redux/appointments/appointmentReducer.js
+++ src/redux/appointments/appointmentReducer.js
@@ -1,8 +1,9 @@
-import CREATE_APPOINTMENT from './actions/types';
+import { CREATE_APPOINTMENT, GET_APPOINTMENTS, DELETE_APPOINTMENT } from './actions/types';
 
 const initialState = {
   fetchedData: '',
   status: '',
+  appointments: [],
 };
 
 const appointmentReducer = (state = initialState, action) => {
@@ -13,6 +14,16 @@ const appointmentReducer = (state = initialState, action) => {
         fetchedData: action.payload.response,
         status: action.payload.status,
       };
+      case GET_APPOINTMENTS:
+        return {
+          ...state,
+          appointments: action.payload
+        };
+        case DELETE_APPOINTMENT:
+          return {
+            ...state,
+            appointments: state.appointments.filter(appointment => appointment.id !== action.payload)
+          }
     default:
       return state;
   }
